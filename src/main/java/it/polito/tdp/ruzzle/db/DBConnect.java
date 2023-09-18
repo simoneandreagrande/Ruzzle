a4a4aa3778ae672f300f84db5c9753762b07eafc
simoneandreagrande <simoneandreagrande@gmail.com>
2023-09-18 06:16:04 +0200

    .

--- src/main/java/it/polito/tdp/ruzzle/db/DBConnect.java
+++ src/main/java/it/polito/tdp/ruzzle/db/DBConnect.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DBConnect {
 	
-	static private final String jdbcUrl = "jdbc:mysql://localhost/dizionario?user=root&password=password";
+	static private final String jdbcUrl = "jdbc:mariadb://localhost/dizionario?user=root&password=root";
 
 	public static Connection getConnection() {
 
